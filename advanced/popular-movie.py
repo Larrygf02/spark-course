17be671ca05ffce31979965394a63d0c1b58e597
Larrygf02 <raulgf_02@hotmail.com>
2022-07-10 11:29:38 -0500

    popular movie

---
+++ advanced/popular-movie.py
@@ -0,0 +1,20 @@
+from struct import Struct
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, LongType
+
+spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
+
+schema = StructType([ \
+                StructField("userID", IntegerType(), True), \
+                StructField("movieID", IntegerType(), True), \
+                StructField("rating", IntegerType(), True), \
+                StructField("timestamp", LongType(), True)])
+
+moviesDF = spark.read.option("sep", "\t").schema(schema).csv("ml-100k/u.data")
+
+topMoviesIDs = moviesDF.groupBy("movieID").count().orderBy(func.desc("count"))
+
+topMoviesIDs.show(10)
+
+spark.stop()
