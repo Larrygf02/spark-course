d1fa20046ab599320bf99735ec4f0739fd8ff570
Larrygf02 <raulgf_02@hotmail.com>
2022-08-14 11:54:22 -0500

    popular movie

--- advanced/popular-movie.py
+++ advanced/popular-movie.py
@@ -2,9 +2,20 @@ from struct import Struct
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, LongType
+import codecs
+
+def loadMovieNames():
+    movieNames = {}
+    with codecs.open("ml-100k/u.ITEM", 'r', encoding='ISO-8859-1', errors='ignore') as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
 
 spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
+nameDict = spark.sparkContext.broadcast(loadMovieNames())
 
+# Create schema when reading u.data
 schema = StructType([ \
                 StructField("userID", IntegerType(), True), \
                 StructField("movieID", IntegerType(), True), \
@@ -13,8 +24,22 @@ schema = StructType([ \
 
 moviesDF = spark.read.option("sep", "\t").schema(schema).csv("ml-100k/u.data")
 
-topMoviesIDs = moviesDF.groupBy("movieID").count().orderBy(func.desc("count"))
+movieCounts = moviesDF.groupBy("movieID").count()
 
-topMoviesIDs.show(10)
+def lookupName(movieID):
+    return nameDict.value[movieID]
+
+lookupNameUDF = func.udf(lookupName)
+
+moviesWithName = movieCounts.withColumn('movieTitle', lookupNameUDF(func.col('movieID')))
+
+sortedMoviesWithName = moviesWithName.orderBy(func.desc("count"))
+
+sortedMoviesWithName.show(10, False)
 
 spark.stop()
+# topMoviesIDs = moviesDF.groupBy("movieID").count() #.orderBy(func.desc("count"))
+
+# topMoviesIDs.show(10)
+
+# spark.stop()
