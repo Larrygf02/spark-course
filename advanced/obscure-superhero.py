588ff61b4fbfb12063d3a33df6f9b8ab166265f3
Larrygf02 <raulgf_02@hotmail.com>
2022-08-14 12:37:48 -0500

    obscure superhero

---
+++ advanced/obscure-superhero.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+spark = SparkSession.builder.appName("MostPopularHero").getOrCreate()
+
+schema = StructType([ \
+                    StructField("id", IntegerType(), True), \
+                    StructField("name", StringType(), True)])
+
+names = spark.read.schema(schema).option("sep", " ").csv("Marvel+Names.txt")
+
+lines = spark.read.text("Marvel+Graph.txt")
+
+connections = lines.withColumn("id", func.split(func.col("value"), " ")[0]) \
+    .withColumn("connections", func.size(func.split(func.col("value"), " ")) - 1) \
+    .groupBy("id").agg(func.sum("connections").alias("connections"))
+
+minConnectionCount = connections.agg(func.min("connections")).first()[0]
+
+minConnections = connections.filter(func.col("connections") == minConnectionCount)
+minConnectionsWithNames = minConnections.join(names, "id")
+
+print("The following characters have only " + str(minConnectionCount) + " connection(s):")
+
+minConnectionsWithNames.select("name").show()
